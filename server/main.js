530d9ca52cfd2d7abcc53f56757f3ccb7e5e0604
mikel evins <mikel@evins.net>
2021-10-10 12:54:47 -0500

    added a strawman (but functioning) websocket server in Electron

--- server/main.js
+++ server/main.js
@@ -1,8 +1,48 @@
 // the presentation server main process
 
 const { app, BrowserWindow } = require('electron')
-const http                   = require('http');
-const WebSocket              = require('ws');
+
+const http = require('http');
+const fs = require('fs');
+const ws = new require('ws');
+
+const clients = new Set();
+
+function accept(req, res) {
+
+  if (req.url == '/ws' && req.headers.upgrade &&
+      req.headers.upgrade.toLowerCase() == 'websocket' &&
+      // can be Connection: keep-alive, Upgrade
+      req.headers.connection.match(/\bupgrade\b/i)) {
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+  } else if (req.url == '/') { // index.html
+    fs.createReadStream('./index.html').pipe(res);
+  } else { // page not found
+    res.writeHead(404);
+    res.end();
+  }
+}
+
+function onSocketConnect(ws) {
+  clients.add(ws);
+  log(`new connection`);
+
+  ws.on('message', function(message) {
+    log(`message received: ${message}`);
+
+    message = message.slice(0, 50); // max message length will be 50
+
+    for(let client of clients) {
+      client.send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    log(`connection closed`);
+    clients.delete(ws);
+  });
+}
+
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -13,19 +53,23 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+function startWS() {
+    httpServer = http.createServer(accept).listen(8080);
+    wsServer =  new ws.Server({server: httpServer});
+}
+
+app.whenReady().then(startWS).then(createWindow)
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-app.whenReady().then(() => {
-  createWindow()
+// for macos: create a new window when activating the app of no windows exist
+// app.whenReady().then(() => {
+//   createWindow()
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+//   app.on('activate', function () {
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// })
 
