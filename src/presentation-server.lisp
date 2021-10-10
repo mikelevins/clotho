69b8ecfefa3eac6987c1d514bc8eee6b3f39fa11
mikel evins <mikel@evins.net>
2021-10-10 15:53:03 -0500

    added (simpleminded) code to launch and kill the Electron process

--- src/presentation-server.lisp
+++ src/presentation-server.lisp
@@ -2,4 +2,15 @@
 
 (in-package #:presentation-server)
 
+(defparameter *presentation-server* nil)
 
+(defun launch-presentation-server ()
+  (let* ((exepath "server/presentation-server-darwin-x64/presentation-server.app/Contents/MacOS/presentation-server")
+         (server-path (namestring (asdf:system-relative-pathname :presentation-server exepath))))
+    (setf *presentation-server* (uiop:launch-program server-path))))
+
+(defun terminate-presentation-server ()
+  (uiop:terminate-process *presentation-server*))
+
+#+(or nil)(launch-presentation-server)
+#+(or nil)(terminate-presentation-server)
