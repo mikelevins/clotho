2c97838604f9c649b42e959a10576011bffa9ddd
mikel evins <mikel@evins.net>
2021-10-10 10:35:43 -0500

    added main.js and index.html; the app now launches its window

---
+++ server/main.js
@@ -0,0 +1,29 @@
+// the presentation server main process
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
