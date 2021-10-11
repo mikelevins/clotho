6c66ee4adaad44fe552e76d4c7254e18c530b092
mikel evins <mikel@evins.net>
2021-10-10 21:18:32 -0500

    added win32-specific code

--- src/presentation-server.lisp
+++ src/presentation-server.lisp
@@ -4,13 +4,20 @@
 
 (defparameter *presentation-server* nil)
 
+#+darwin
 (defun launch-presentation-server ()
   (let* ((exepath "server/presentation-server-darwin-x64/presentation-server.app/Contents/MacOS/presentation-server")
          (server-path (namestring (asdf:system-relative-pathname :presentation-server exepath))))
     (setf *presentation-server* (uiop:launch-program server-path))))
 
+#+win32
+(defun launch-presentation-server ()
+  (let* ((exepath "server/presentation-server-win32-x64/presentation-server.exe")
+         (server-path (namestring (asdf:system-relative-pathname :presentation-server exepath))))
+    (setf *presentation-server* (uiop:launch-program server-path))))
+
 (defun terminate-presentation-server ()
-  (uiop:terminate-process *presentation-server*))
+  (uiop:terminate-process *presentation-server* :urgent t))
 
 #+(or nil)(launch-presentation-server)
-#+(or nil)(terminate-presentation-server)
+#+(or nil)(terminate-presentation-server )
