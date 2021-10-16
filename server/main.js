cdba36af8a02b3590d13cf68b1d7a31bafb4893e
mikel evins <mikel@evins.net>
2021-10-16 16:52:13 -0500

    removed websocket server from the Javascript side

--- server/main.js
+++ server/main.js
@@ -4,45 +4,8 @@ const { app, BrowserWindow } = require('electron')
 
 const http = require('http');
 const fs = require('fs');
-const ws = new require('ws');
 
 const clients = new Set();
-
-function accept(req, res) {
-
-  if (req.url == '/ws' && req.headers.upgrade &&
-      req.headers.upgrade.toLowerCase() == 'websocket' &&
-      // can be Connection: keep-alive, Upgrade
-      req.headers.connection.match(/\bupgrade\b/i)) {
-    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-  } else if (req.url == '/') { // index.html
-    fs.createReadStream('./index.html').pipe(res);
-  } else { // page not found
-    res.writeHead(404);
-    res.end();
-  }
-}
-
-function onSocketConnect(ws) {
-  clients.add(ws);
-  log(`new connection`);
-
-  ws.on('message', function(message) {
-    log(`message received: ${message}`);
-
-    message = message.slice(0, 50); // max message length will be 50
-
-    for(let client of clients) {
-      client.send(message);
-    }
-  });
-
-  ws.on('close', function() {
-    log(`connection closed`);
-    clients.delete(ws);
-  });
-}
-
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -53,12 +16,7 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
-function startWS() {
-    httpServer = http.createServer(accept).listen(8080);
-    wsServer =  new ws.Server({server: httpServer});
-}
-
-app.whenReady().then(startWS).then(createWindow)
+app.whenReady().then(createWindow)
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
