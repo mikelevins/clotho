ff7cb704ea09486396b4d79530839d761cfebb29
mikel evins <mikel@evins.net>
2021-10-24 06:04:43 -0500

    minor edit

--- src/http.lisp
+++ src/http.lisp
@@ -9,7 +9,8 @@
    (:head
     (:meta :charset "utf-8")
     (:meta :http-equiv "X-UA-Compatible" :content "IE=edge")
-    (:meta :name "viewport" :content "width=device-width, initial-scale=1"))
+    (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+    (:title "Clotho"))
    (:body
     (:script (cl-markup:raw (remote-js:js *remote-js-context*))))))
 
